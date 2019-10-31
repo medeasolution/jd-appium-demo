e5925f12a6eefbf173a118dac04a19546ee71c02
Marc Sances <msances@wearejust.digital>
2019-10-31 11:01:13 +0100

    3-get-going

--- features/calculator.feature
+++ features/calculator.feature
@@ -7,4 +7,12 @@ Feature: Calculator
     Given   the app is open
 
   Scenario: Zero is displayed when the calculator starts
-    Then    the "result" label displays the value "1"
+    Then    the "result" label displays the value "0"
+
+  Scenario: Operation 15 plus 20 has result 35
+    Given   the "result" label displays the value "0"
+    When    I input numbers "15"
+    And     I press "plus" button
+    And     I input numbers "20"
+    And     I press "equals" button
+    Then    the "result" label displays the value "35"
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -14,3 +14,15 @@ def open_app(context):
 def label_displays_value(context, name, value):
     element = context.driver.find_element_by_id('result')
     assert element.text == value, "Values for {} are not equal, expect {} but have {}".format(name, value, element.text)
+
+
+@step('I press "{button}" button')
+def press_button(context, button):
+    element = context.driver.find_element_by_id("btn_" + button)
+    element.click()
+
+
+@step('I input numbers "{number}"')
+def input_numbers(context, number):
+    for digit in number:
+        press_button(digit)
